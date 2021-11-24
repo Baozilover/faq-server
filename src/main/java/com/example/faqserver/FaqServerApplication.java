7f58c0e7de33e66ebd080786233a1190af0b8b83
BaoziLover <16281130@bjtu.edu.cn>
2021-11-24 22:10:33 +0800

    start

---
+++ src/main/java/com/example/faqserver/FaqServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.faqserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqServerApplication.class, args);
+	}
+
+}
